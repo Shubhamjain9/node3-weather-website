10aa96acea0f4e78285016ffc05c4ae867f7e99c
unknown <shubham9415jain@gmail.com>
2019-09-25 01:04:26 +0530

    Init Command

---
+++ src/app.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const hbs = require('hbs')
+
+
+
+// Define path for express config
+const publicDirPath  = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialPath = path.join(__dirname,'../templates/partials')
+
+// Define handlebar engine and view location
+app.set('view engine','hbs')
+app.set('views',viewsPath) 
+hbs.registerPartials(partialPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirPath))
+
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'Weather app',
+        name:'Shubham Jain'
+    })
+})
+
+app.get('/about',(req,resp)=>{
+    resp.render('about',{
+        title:' About Weather',
+        name:'Shubham Jain'
+    })
+})
+
+app.get('/help',(req,resp)=>{
+    resp.render('help',{
+        title:'Help Section',
+        name : 'Shubham Jain',
+        MobileNo : 9630833902
+    })
+})
+
+app.get('/weather',(req,res)=>{
+    res.send({
+        forecast : 'Cloudy',
+        location : 'Ujjian'
+    })
+})
+
+app.get('/help/*',(req,res)=>{
+    res.render('404',{
+        title:404,
+        name : 'Shubham Jain',
+        errorMessage : 'Help article not found'
+    })
+    // res.send('Help Article not found')
+})
+
+app.get('*',(req,res)=>{
+    // res.send('404 page')
+    res.render('404',{
+        title:404,
+        name:'Shubham Jain',
+        errorMessage:'Page not found'
+    })
+})
+
+app.listen(3000,()=>{
+    console.log('Server is up on port 3000')
+})
+
+
+
+
+
+
